359c83f0c04984454c1b7167aa0caf039ba20ae2
jhes <ooxfordck@gmail.com>
2020-06-19 21:40:21 +0800

    modified changes

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-echo "====================="
+echo "========================="
 echo "Builing for production..."
-echo "====================="
-cd /Users/hesk/Documents/ixo/dpexplorer
-meteor build ./build/ --architecture os.linux.x86_64 --server-only --allow-superuser
+echo "========================="
+cd $HOME/Documents/ixo/dpexplorer
+BUILD_DIR=$HOME/Documents/ixo/dpexplorer/build
+#meteor build ./build/ --architecture os.linux.x86_64 --server-only --allow-superuser
 #meteor build --architecture=os.linux.x86_64 ../build
 LOCAL=8.210.117.181
-EXTRACT="tar xvf dp-explorer.tar.gz -C /www/wwwdp"
-scp ./build/dp-explorer.tar.gz root@$LOCAL:/www/wwwdp
-ssh -t root@$LOCAL $EXTRACT
+#EXTRACT="cd /www/wwwdp;bash tar -xvf dpexplorer.tar.gz -C /www/wwwdp"
+EXTRACT="cd /www/wwwdp; tar -xvf dpexplorer.tar.gz;bash"
+if [ $USER  == 'hesk' ] 
+then
+	scp $BUILD_DIR/dpexplorer.tar.gz root@$LOCAL:/www/wwwdp
+	ssh -t root@$LOCAL $EXTRACT
+	echo "local"
+fi
 #cd bundle/programs/server && npm install
