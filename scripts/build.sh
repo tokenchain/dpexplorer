ddc70045c1584dcba4f1098acee7a3834cb23448
jhes <ooxfordck@gmail.com>
2020-06-19 21:17:16 +0800

    modified changes

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,10 +2,11 @@
 echo "====================="
 echo "Builing for production..."
 echo "====================="
+cd /Users/hesk/Documents/ixo/dpexplorer
 meteor build ./build/ --architecture os.linux.x86_64 --server-only --allow-superuser
-
 #meteor build --architecture=os.linux.x86_64 ../build
-
-#scp ../build/meteor-build-test.tar.gz root@your.server.ip.address:/home/meteor/build
-#tar xvf meteor-build-test.tar.gz
+LOCAL=8.210.117.181
+EXTRACT="tar xvf dp-explorer.tar.gz -C /www/wwwdp"
+scp ./build/dp-explorer.tar.gz root@$LOCAL:/www/wwwdp
+ssh -t root@$LOCAL $EXTRACT
 #cd bundle/programs/server && npm install
--- scripts/service.sh
+++ scripts/service.sh
@@ -2,4 +2,4 @@
 export MONGO_URL='mongodb://admin:admin@localhost:27017/syncdp'
 export ROOT_URL='http://darkpool.profitlock.io'
 export MAIL_URL='smtp://admin:admin@localhost:9999/'
-node main.js
+node main.js --setting settings_local.json
