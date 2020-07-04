523db2aff4c8fe941210a0267ee442638aad73f8
jhes <ooxfordck@gmail.com>
2020-07-05 01:16:39 +0800

    new shell script helpers

--- scripts/local.sh
+++ scripts/local.sh
@@ -4,8 +4,76 @@
 #IP_1=8.210.117.181
 #SETTING=settings.json
 #cat $SETTING|jq '.remote.lcd = "http://'$BOND_DID'"' -c $SETTING | sponge $SETTING
+
+env_segment(){
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    echo "mainnet"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    # Mac OSX
+   echo "testlocal"
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+    echo "testlocal"
+    # POSIX compatibility layer and Linux environment emulation for Windows
+elif [[ "$OSTYPE" == "msys" ]]; then
+    # Windows
+   echo "testlocal"
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+    # ...
+    echo "testlocal"
+fi
+}
+
+env_setting_file(){
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    echo "settings_centos.json"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    # Mac OSX
+   echo "settings_local.json"
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+    echo "settings_local.json"
+    # POSIX compatibility layer and Linux environment emulation for Windows
+elif [[ "$OSTYPE" == "msys" ]]; then
+    # Windows
+   echo "settings_local.json"
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+    # ...
+    echo "settings_local.json"
+fi
+}
+
+
+CLI_PATH="$HOME/.dpcli"
+DEMON_PATH="$HOME/.dpd"
+JSONFile=$DEMON_PATH/config/genesis.json
+
+RPC=http://127.0.0.1:26657
+LCD=http://127.0.0.1:1317
+
+SEGMENT=$(env_segment)
+JSON_TARGET="https://gitee.com/jjhoc/b-explorer-settings/raw/master/v1.3/$SEGMENT/genesis.json"
+JSON_GIT_PATH="$HOME/Documents/ixo/b-explorer-settings/v1.3/$SEGMENT/genesis.json"
+EXPLORER_SETTINGS=$(env_setting_file)
+
+
+echo "==========================="
+echo "version genesis update"
+echo "==========================="
+cp $JSONFile $JSON_GIT_PATH
+cd $HOME/Documents/ixo/b-explorer-settings
+sh pushcommit.sh
+
+cd $HOME/Documents/ixo/dpexplorer
+
+echo "====================================="
+echo "if do not have sponge please install. MacOS please run [brew install moreutil]. "
+echo "====================================="
+
+cat $EXPLORER_SETTINGS|jq '.genesisFile = "'$JSON_TARGET'"' -c $EXPLORER_SETTINGS | sponge $EXPLORER_SETTINGS
+cat $EXPLORER_SETTINGS|jq '.remote.rpc = "'$RPC'"' -c $EXPLORER_SETTINGS | sponge $EXPLORER_SETTINGS
+cat $EXPLORER_SETTINGS|jq '.remote.lcd = "'$LCD'"' -c $EXPLORER_SETTINGS | sponge $EXPLORER_SETTINGS
+
 echo "==========================="
 echo "Starting the server now..."
 echo "==========================="
-cd $HOME/Documents/ixo/dpexplorer
-meteor --settings settings_local.json
+
+meteor --settings $EXPLORER_SETTINGS
